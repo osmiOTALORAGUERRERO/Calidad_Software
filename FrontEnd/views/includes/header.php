aeffec199e1bce225df40ad8ec2c6c3f68559e40
Michael9911 <mpulidoreye@uniminuto.edu.co>
2019-05-06 22:55:08 -0500

    Icono del header (pestaña)

--- FrontEnd/views/includes/header.php
+++ FrontEnd/views/includes/header.php
@@ -5,6 +5,7 @@
   <meta charset="utf-8">
   <title>Inmigrantes</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="icon" href="https://daytradingacademy.co/wp-content/uploads/2018/05/0003467-flagge-kolumbien.png">
 </head>
 
 <body>
